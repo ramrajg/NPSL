b21266d9e42ce06511c558b0e1e3c9d4ef6a124d
Ramraj <ramrajg@live.com>
2018-09-23 23:09:01 +0530

    Few More Changes

--- NPSLCore/NPSL.Models/Models/Dashboard.cs
+++ NPSLCore/NPSL.Models/Models/Dashboard.cs
@@ -6,5 +6,6 @@
         public string TemplateGroupName { get; set; }
         public decimal ConsiledAmount { get; set; }
         public decimal NonConsiledAmount { get; set; }
+        public decimal PrimaryAmount { get; set; }
     }
 }
