512ba3a8a430da13f44f39f2e1fa54e11db265b5
Ramraj <ramrajg@live.com>
2018-10-28 02:56:45 +0530

    Added Option for XLS and XLSX File

--- Reconsile_Service/Reconsile_Service/ReconclieProcessing.cs
+++ Reconsile_Service/Reconsile_Service/ReconclieProcessing.cs
@@ -1,7 +1,10 @@
 ﻿using NPSLCore.Models.DB;
+using OfficeOpenXml;
+using Spire.Xls;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -61,7 +64,8 @@ namespace ReconsileProcess
                             {
                                 Console.ForegroundColor = ConsoleColor.Green;
                                 Console.WriteLine("Sucessfully Reconsiled - Template Name " + item.TemplateName + " on : " + DateTime.Now);
-                            }else { Log("Sucessfully Reconsiled - Template Name " + item.TemplateName + " on : " + DateTime.Now); }
+                            }
+                            else { Log("Sucessfully Reconsiled - Template Name " + item.TemplateName + " on : " + DateTime.Now); }
                         }
                     }
                     catch (Exception ex)
@@ -88,6 +92,8 @@ namespace ReconsileProcess
             string[] strArr = SubstringValue.Split('|');
             NumberofColumns = strArr.Length;
             CreateDirectory(MoveFilepath);
+            string[] formats = getSuppotedDateFormat();
+            DateTime dateValue;
             foreach (string dirFile in Files)
             {
                 try
@@ -108,65 +114,180 @@ namespace ReconsileProcess
                             Console.WriteLine("READING " + fileName + " File");
                         }
                         else { Log("READING " + fileName + " File"); }
-                        using (var fileStream = File.OpenRead(dirFile))
-                        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+                        if (ext.ToLower() == ".xls" || ext.ToLower() == ".xlsx")
                         {
-                            if (HasHeader == true) streamReader.ReadLine();
-                            String line;
-                            while ((line = streamReader.ReadLine()) != null)
+                            var xlsxFilePath = "";
+                            if (ext.ToLower() == ".xls")
                             {
-                                if (line != "")
+                                xlsxFilePath = ConvertXLS_XLSX(dirFile, MoveFilepath + Path.GetRandomFileName() + ".xlsx");
+                            }
+                            else
+                            {
+                                xlsxFilePath = dirFile;
+                            }
+                            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(xlsxFilePath)))
+                            {
+                                var myWorksheet = xlPackage.Workbook.Worksheets.First(); //select sheet here
+                                var totalRows = myWorksheet.Dimension.End.Row;
+                                var totalColumns = myWorksheet.Dimension.End.Column;
+
+                                var sb = new StringBuilder(); //this is your your data
+                                for (int rowNum = Int32.Parse(Delimeter) + 1; rowNum <= totalRows; rowNum++) //selet starting row here
                                 {
                                     substring = "";
-                                    if (Delimeter == "SS")
+                                    var row = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString());
+                                    sb.AppendLine(string.Join(",", row));
+                                    for (int i = 0; i < strArr.Length; i++)
                                     {
-                                        for (int i = 0; i < strArr.Length; i++)
+                                        if (strArr[i] != "")
                                         {
-                                            if (strArr[i] != "")
+                                            if (i == 2)
                                             {
-                                                if (i == 2)
-                                                {
-                                                    var numbers = strArr[i].Split(',').Select(Int32.Parse).ToList();
-                                                    substring = substring + line.Substring(numbers[0]-1, numbers[1]).Replace(".", "") + ",";
-                                                }
-                                                else
-                                                {
-                                                    var numbers = strArr[i].Split(',').Select(Int32.Parse).ToList();
-                                                    substring = substring + line.Substring(numbers[0]-1, numbers[1]) + ",";
-                                                }
+                                                substring = substring + row.ElementAt(Int32.Parse(strArr[i])).Replace(".", "") + ",";
                                             }
-                                            else
+                                            else if (i == 1)
                                             {
-                                                substring = substring + ",";
-                                            }
-                                        }
-                                    }
-                                    else
-                                    {
-                                        var columnValue = line.Split(new string[] { Delimeter }, StringSplitOptions.None).ToList();
-                                        for (int i = 0; i < strArr.Length; i++)
-                                        {
-                                            if (strArr[i] != "")
-                                            {
-                                                if (i == 2)
+                                                if (row.ElementAt(Int32.Parse(strArr[i])) != "")
                                                 {
-                                                    substring = substring + columnValue[Int32.Parse(strArr[i])].Replace(".","") + ",";
-                                                }
-                                                else
-                                                {
-                                                    substring = substring + columnValue[Int32.Parse(strArr[i])] + ",";
-                                                }
-                                            }
-                                            else
-                                            {
-                                                substring = substring + ",";
-                                            }
 
+                                                    DateTime.TryParseExact(row.ElementAt(Int32.Parse(strArr[i])), formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue);
+                                                    substring = substring + dateValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) + ",";
+                                                }
+                                                else
+                                                {
+                                                    DateTime.TryParseExact(DateTime.Now.ToString(), formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue);
+                                                    substring = substring + dateValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) + ",";
+                                                }
+                                            }
+                                            else
+                                            {
+                                                if (row.ElementAt(Int32.Parse(strArr[i])) != "")
+                                                {
+                                                    substring = substring + row.ElementAt(Int32.Parse(strArr[i])) + ",";
+                                                }
+                                                else
+                                                {
+                                                    substring = substring + ",";
+                                                }
+
+                                            }
                                         }
+                                        else
+                                        {
+                                            substring = substring + ",";
+                                        }
+
                                     }
                                     File.AppendAllText(Filepath, substring = substring.TrimEnd(',') + "," + fileNameWithExt + "\n");
                                 }
                             }
+                            if (ext.ToLower() == ".xls")
+                            {
+                                if (File.Exists(xlsxFilePath))
+                                {
+                                    File.Delete(xlsxFilePath);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            using (var fileStream = File.OpenRead(dirFile))
+                            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+                            {
+                                if (HasHeader == true) streamReader.ReadLine();
+                                String line;
+                                while ((line = streamReader.ReadLine()) != null)
+                                {
+                                    if (line != "")
+                                    {
+                                        substring = "";
+                                        if (Delimeter == "SS")
+                                        {
+                                            for (int i = 0; i < strArr.Length; i++)
+                                            {
+                                                if (strArr[i] != "")
+                                                {
+                                                    if (i == 2)
+                                                    {
+                                                        var numbers = strArr[i].Split(',').Select(Int32.Parse).ToList();
+                                                        substring = substring + line.Substring(numbers[0] - 1, numbers[1]).Replace(".", "") + ",";
+                                                    }
+                                                    else if (i == 1)
+                                                    {
+                                                        var numbers = strArr[i].Split(',').Select(Int32.Parse).ToList();
+                                                        if (line.Substring(numbers[0] - 1, numbers[1]) != "")
+                                                        {
+                                                            DateTime.TryParseExact(line.Substring(numbers[0] - 1, numbers[1]), formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue);
+                                                            substring = substring + dateValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) + ",";
+                                                        }
+                                                        else
+                                                        {
+                                                            DateTime.TryParseExact(DateTime.Now.ToString(), formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue);
+                                                            substring = substring + dateValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) + ",";
+                                                        }
+
+                                                    }
+                                                    else
+                                                    {
+                                                        var numbers = strArr[i].Split(',').Select(Int32.Parse).ToList();
+                                                        if (line.Substring(numbers[0] - 1, numbers[1]) != "")
+                                                        {
+                                                            substring = substring + line.Substring(numbers[0] - 1, numbers[1]) + ",";
+                                                        }
+                                                        else { substring = substring + ","; }
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    substring = substring + ",";
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            var columnValue = line.Split(new string[] { Delimeter }, StringSplitOptions.None).ToList();
+                                            for (int i = 0; i < strArr.Length; i++)
+                                            {
+                                                if (strArr[i] != "")
+                                                {
+                                                    if (i == 2)
+                                                    {
+                                                        substring = substring + columnValue[Int32.Parse(strArr[i])].Replace(".", "") + ",";
+                                                    }
+                                                    else if (i == 1)
+                                                    {
+                                                        if (columnValue[Int32.Parse(strArr[i])] != "")
+                                                        {
+                                                            DateTime.TryParseExact(columnValue[Int32.Parse(strArr[i])], formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue);
+                                                            substring = substring + dateValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) + ",";
+                                                        }
+                                                        else
+                                                        {
+                                                            DateTime.TryParseExact(DateTime.Now.ToString(), formats, System.Globalization.CultureInfo.InvariantCulture, DateTimeStyles.None, out dateValue);
+                                                            substring = substring + dateValue.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture) + ",";
+                                                        }
+                                                    }
+                                                    else
+                                                    {
+                                                        if (columnValue[Int32.Parse(strArr[i])] != "")
+                                                        {
+                                                            substring = substring + columnValue[Int32.Parse(strArr[i])] + ",";
+                                                        }
+                                                        else { substring = substring + ","; }
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    substring = substring + ",";
+                                                }
+
+                                            }
+                                        }
+                                        File.AppendAllText(Filepath, substring = substring.TrimEnd(',') + "," + fileNameWithExt + "\n");
+                                    }
+                                }
+                            }
+
                         }
                         if (!File.Exists(MoveFilepath + Path.GetFileName(dirFile)))
                         {
@@ -176,6 +297,7 @@ namespace ReconsileProcess
                         {
                             File.Delete(dirFile);
                         }
+
                     }
                 }
                 catch (Exception ex)
@@ -196,7 +318,14 @@ namespace ReconsileProcess
             }
 
         }
+        public static string ConvertXLS_XLSX(string file,string xlsxPath)
+        {
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(file);
+            workbook.SaveToFile(xlsxPath, ExcelVersion.Version2013);
 
+            return xlsxPath;
+        }
         static bool IsFileLocked(FileInfo file)
         {
             FileStream stream = null;
@@ -244,6 +373,23 @@ namespace ReconsileProcess
                 log.WriteLine();
                 log.Close();
             }
+        }
+        private string[] getSuppotedDateFormat()
+        {
+            string[] DateFormateList =  { "M/d/yyyy h:mm:ss tt", "M/d/yyyy h:mm tt",
+                     "MM/dd/yyyy hh:mm:ss", "M/d/yyyy h:mm:ss",
+                     "M/d/yyyy hh:mm tt", "M/d/yyyy hh tt",
+                     "M/d/yyyy h:mm", "M/d/yyyy h:mm",
+                     "MM/dd/yyyy hh:mm", "M/dd/yyyy hh:mm","yyyyMMdd","dd-MM-yyyy hh:mm:ss","dd-MM-yyyy HH:mm: ss"};
+            try
+            {
+                DateFormateList = File.ReadAllLines("DateFormatSupported.txt");
+            }
+            catch (Exception ex)
+            {
+                return DateFormateList;
+            }
+            return DateFormateList;
         }
     }
 }
